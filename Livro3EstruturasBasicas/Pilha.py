514b57c8ea9d2c497948841d897deea9c3cc3287
Johann <johannhomonnai@gmail.com>
2021-08-11 15:36:03 -0300

    Added Pilha.py
    Implementation for the stack data structure.

---
+++ Livro3EstruturasBasicas/Pilha.py
@@ -0,0 +1,24 @@
+class Pilha: # considerando que o topo é o fim da lista
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+        # se o topo fosse o começo da lista:
+        # self.items.insert(0, item)
+
+    def pop(self):
+        return self.items.pop()
+        # se o topo fosse o começo da lista:
+        # self.items.pop(0)
+
+    def peek(self):
+        return self.items[-1]
+        # se o topo fosse o começo da lista:
+        # self.items[0]
+
+    def size(self):
+        return len(self.items)   
