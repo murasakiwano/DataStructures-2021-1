acf73b386761e62b3d0017a05d67969296a0709a
Johann <johannhomonnai@gmail.com>
2021-08-11 17:24:10 -0300

    Added checkParens.py and checkSymbol.py
    Programs that check if a string of symbols (`([{}])`) is balanced.

---
+++ Livro3EstruturasBasicas/checkParens.py
@@ -0,0 +1,23 @@
+# Função para checar se os parênteses estão balanceados
+from Pilha import Pilha
+
+def checkParens(symbolString):
+    s = Pilha()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol == "(":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                s.pop()
+
+        index = index + 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
---
+++ Livro3EstruturasBasicas/checkSymbol.py
@@ -0,0 +1,31 @@
+# Função para checar se os parênteses estão balanceados
+from Pilha import Pilha
+
+def checkSymbol(symbolString):
+    s = Pilha()
+    balanced = True
+    index = 0
+    while index < len(symbolString) and balanced:
+        symbol = symbolString[index]
+        if symbol in "([{":
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                balanced = False
+            else:
+                top = s.pop()
+                if not matches(top,symbol):
+                    balanced = False
+
+        index = index + 1
+
+    if balanced and s.isEmpty():
+        return True
+    else:
+        return False
+
+def matches(open, close):
+    opens = "([{"
+    closes = ")]}"
+
+    return opens.index(open) == closes.index(close)
