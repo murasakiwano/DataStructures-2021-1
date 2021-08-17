9b2d29d654817cd9a1e2430bb7707198fd136d05
Johann <johannhomonnai@gmail.com>
2021-08-17 09:12:44 -0300

    Added hotPotato: good solution to Flavius Josephus problem

---
+++ Livro3EstruturasBasicas/hotPotato.py
@@ -0,0 +1,19 @@
+# Implementação da simulação de Batata Quente.
+# Também pode ser considerada uma versão do problema clássico Flavius Josephus.
+
+from basicDS import Queue
+
+def hotPotato(namelist, num):
+    simqueue = Queue()
+    for name in namelist:
+        simqueue.enqueue(name)
+
+    while simqueue.size() > 1:
+        for i in range(num):
+            simqueue.enqueue(simqueue.dequeue())
+
+        simqueue.dequeue()
+
+    return simqueue.dequeue()
+
+print(hotPotato(["Jojo", "Lulu", "Diego", "Mixirica", "Ismael", "Vinius"], 9))
