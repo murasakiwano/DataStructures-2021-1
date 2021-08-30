8951a23c0373eba82de7483434e2bfdae664a24c
Johann <johannhomonnai@gmail.com>
2021-08-29 23:37:11 -0300

    Added blocos_de_Pilha.py

---
+++ Livro4Recursao/blocos_de_Pilha.py
@@ -0,0 +1,18 @@
+from Livro3EstruturasBasicas.DataStructuresChapter3.Stack import Stack
+
+rStack = Stack()
+
+def toStr(n,base):
+    convertString = "0123456789ABCDEF"
+    while n > 0:
+        if n < base:
+            rStack.push(convertString[n])
+        else:
+            rStack.push(convertString[n % base])
+        n = n // base
+    res = ""
+    while not rStack.isEmpty():
+        res = res + str(rStack.pop())
+    return res
+
+print(toStr(1453,16))
