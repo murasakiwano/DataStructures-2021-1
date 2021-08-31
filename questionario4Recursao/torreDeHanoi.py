99921de4fcd9a98120d6ca3a7f48c8c05213fd8b
Johann <johannhomonnai@gmail.com>
2021-08-31 17:06:16 -0300

    Só mudando nome de variáveis

--- questionario4Recursao/torreDeHanoi.py
+++ questionario4Recursao/torreDeHanoi.py
@@ -1,10 +1,7 @@
 # Questão 1 - Torre de Hanoi
 # Input: N A C B
-# N é o número de discos
-# A é o pole inicial
-# C é o pole final
-# B é o pole intermediário
-N, A, C, B = input().split()
+# N é o número de discos\
+N, origem, destino, intermediario = input().split()
 
 N = int(N)
 
@@ -17,4 +14,4 @@ def moverTorre(altura, origem, destino, intermediario):
 def moverDisco(origem, destino):
     print(f"Mover de {origem} para {destino}.")
 
-moverTorre(N, A, C, B)
+moverTorre(N, origem, destino, intermediario)
