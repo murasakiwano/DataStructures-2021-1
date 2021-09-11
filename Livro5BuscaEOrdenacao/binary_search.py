b38162c7223d79790df3caeca7bdabbabc814190
Johann <johannhomonnai@gmail.com>
2021-09-11 14:40:10 -0300

    Atualizei binary_search.py

--- Livro5BuscaEOrdenacao/binary_search.py
+++ Livro5BuscaEOrdenacao/binary_search.py
@@ -11,6 +11,25 @@ def binarySearch(alist, item):
             else:
                 return binarySearch(alist[midpoint+1:], item)
 
-testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42,]
+
+def binarySearch2(alist, item, first, last):
+    if last >= first:
+        midpoint = (first+last) // 2
+
+        if alist[midpoint] == item:
+            return True
+
+        else:
+            if item < alist[midpoint]:
+                return binarySearch2(alist, item, first, midpoint-1)
+            else:
+                return binarySearch2(alist, item, midpoint+1, last)
+    else:
+        return False
+
+
+testlist = [0, 1, 2, 8, 13, 17, 19, 32, 42, ]
 print(binarySearch(testlist, 3))
 print(binarySearch(testlist, 13))
+print(binarySearch2(testlist, 3, 0, len(testlist)-1))
+print(binarySearch2(testlist, 13, 0, len(testlist)-1))
